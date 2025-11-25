854ce97e191cfd99691a00b44037c09ea7bf637e
Your Name <evangelista.luther1995@gmail.com>
2025-11-25 20:18:27 +1000

    style(Button): make button text smaller for better visual clarity

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "disbaled:cursor-not-allowed inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 ";
+    "text-sm disbaled:cursor-not-allowed inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 ";
 
   const styles = {
     primary: base + "px-4 py-3 md:px-6 md:py-4",
     small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs md:text-md",
     secondary:
-      "px-4 py-2.5 md:px-6 md:py-3.5 border-2 border-stone-300 disbaled:cursor-not-allowed inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:text-stone-600 focus:text-stone-600 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2",
+      "px-4 text-sm py-2.5 md:px-6 md:py-3.5 border-2 border-stone-300 disbaled:cursor-not-allowed inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:text-stone-600 focus:text-stone-600 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2",
   };
 
   if (to)
