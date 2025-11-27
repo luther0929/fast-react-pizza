9c41ead05a12f66d71c0b70d8f715fd60553cc85
Your Name <evangelista.luther1995@gmail.com>
2025-11-27 20:47:32 +1000

    feat(cart): add increase and decrease buttons on UI

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -9,6 +9,7 @@ import spinaci from "../../assets/spinaci.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 const fallbackImages = [
   focaccia,
@@ -64,7 +65,13 @@ function MenuItem({ pizza, index }) {
               Sold out
             </p>
           )}
-          {isInCart && <DeleteItem id={id} />}
+
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity id={id} currentQuantity={currentQuantity} />
+              <DeleteItem id={id} />{" "}
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
