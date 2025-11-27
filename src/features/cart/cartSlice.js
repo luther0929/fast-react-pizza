d90a96b3830c3a4c22a3903d7ec5b20aa7c940b5
Your Name <evangelista.luther1995@gmail.com>
2025-11-27 18:51:49 +1000

    feat(cart): implement getter functions for total cart price and quantity

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,18 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  // cart: [],
-
-  cart: [
-    //dummy data for development testing purposes
-    {
-      pizzaId: 12,
-      name: "Meat Lovers",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
@@ -52,3 +41,9 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
