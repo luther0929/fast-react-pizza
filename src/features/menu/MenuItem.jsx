38fd4349e05bfef6d424528e01ecb08160e23f2e
Your Name <evangelista.luther1995@gmail.com>
2025-11-27 18:13:32 +1000

    feat(cart): add handleAddToCart logic for adding new item

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -6,6 +6,8 @@ import margherita from "../../assets/margherita.jpg";
 import prosciutto from "../../assets/prosciutto.jpg";
 import salamino from "../../assets/salamino.jpg";
 import spinaci from "../../assets/spinaci.jpg";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 const fallbackImages = [
   focaccia,
@@ -21,7 +23,20 @@ function getRotatingFallback(index) {
 }
 
 function MenuItem({ pizza, index }) {
-  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-6">
@@ -48,9 +63,11 @@ function MenuItem({ pizza, index }) {
             </p>
           )}
 
-          <Button type="small" disabled={soldOut}>
-            Add to cart
-          </Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
