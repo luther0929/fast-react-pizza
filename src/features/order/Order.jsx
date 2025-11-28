fd02fc175a4f7e0deac66f42de6dc1fa5800eb02
Your Name <evangelista.luther1995@gmail.com>
2025-11-29 00:14:38 +1000

    feat(order): add make priority button to make order a priority

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -22,8 +23,6 @@ function Order() {
     },
     [fetcher],
   );
-
-  console.log(fetcher.data);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -92,6 +91,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
